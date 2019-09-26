746f204b9e7232393e4e1b69608f34fb4ba908fb
Wildhevire <wildhevire404@gmail.com>
2019-09-26 00:52:31 +0000

    finished

---
+++ src/latihan/variable/Latihan2.java
@@ -0,0 +1,31 @@
+package latihan.variable;
+
+public class Latihan2 {
+	/*
+    Nama        : Wildan Muhammad Fikri
+    Kelas       : IF1
+    NIM         : 10118044
+    Deskripsi   : Contoh deklarasi variabel
+     */
+
+	public static void main(String[] args) {
+        // Deklarasi Variabel
+        int nilaiInt;
+        final double PHI = 3.14;
+        boolean nilaiLogika;
+        char nilaiKarakter;
+
+        // Memberi nilai ke variabel
+
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+
+        // Menampilkan Hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("Isi variavel karakter = " + nilaiKarakter);;
+    }
+}
